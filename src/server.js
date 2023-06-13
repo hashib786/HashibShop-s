9ac1ef25e34b91d811dcf4bd367db367ab734b87
Hashib Raja <hashibraja23@gmail.com>
2023-06-13 12:27:00 +0530

    Changing Server URL

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-// export const server = "https://hashib-s-shop-backend.onrender.com/api/v2";
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://hashib-s-shop-backend.onrender.com/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-// export const backend_url = "https://hashib-s-shop-backend.onrender.com";
-export const backend_url = "http://localhost:8000";
+export const backend_url = "https://hashib-s-shop-backend.onrender.com";
+// export const backend_url = "http://localhost:8000";
